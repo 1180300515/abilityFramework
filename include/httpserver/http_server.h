90ccefea69d433dd62d20a9ff68ec1248e60c153
1180300515 <1516648631@qq.com>
2023-07-31 21:44:33 +0800

    去除不相关部分，格式优化

--- include/httpserver/http_server.h
+++ include/httpserver/http_server.h
@@ -8,15 +8,13 @@
 #include "resource_manager.h"
 #include "lifecycle_manager.h"
 #include "ability_relation_manager.h"
-#include "connection_manager.h"
 
 class HttpServer
 {
 public:
     void Init(std::shared_ptr<ResourceManager> resource_,
               std::shared_ptr<LifeCycleManager> lifecycle_,
-              std::shared_ptr<AbilityRelationManager> relation_,
-              std::shared_ptr<ConnectionManager> connection_);
+              std::shared_ptr<AbilityRelationManager> relation_);
     void Run();
 
 private:
@@ -24,7 +22,6 @@ private:
     std::shared_ptr<ResourceManager> resource_manager_;
     std::shared_ptr<LifeCycleManager> lifecycle_manager_;
     std::shared_ptr<AbilityRelationManager> relation_manager_;
-    std::shared_ptr<ConnectionManager> connection_manager_;
 };
 
 #endif // _HTTP_SERVER_H
--- src/httpserver/http_server.cpp
+++ src/httpserver/http_server.cpp
@@ -4,74 +4,61 @@
 
 #include "color.h"
 
-void HttpServer::Init(std::shared_ptr<ResourceManager> resource_, std::shared_ptr<LifeCycleManager> lifecycle_,std::shared_ptr<AbilityRelationManager> relation_,std::shared_ptr<ConnectionManager> connection_)
+void HttpServer::Init(std::shared_ptr<ResourceManager> resource_,
+                      std::shared_ptr<LifeCycleManager> lifecycle_,
+                      std::shared_ptr<AbilityRelationManager> relation_)
 {
     LOG(INFO) << L_GREEN << "init http server" << NONE;
     this->server = std::make_shared<httplib::Server>();
     this->resource_manager_ = resource_;
     this->lifecycle_manager_ = lifecycle_;
     this->relation_manager_ = relation_;
-    this->connection_manager_ = connection_;
-    this->server->Post("/heartbeat", [this](const httplib::Request &req, httplib::Response &res)
-    {
-        LOG(INFO) << L_BLUE << "httpserver receive heartbeat" << NONE;
-        int port;
-        try {
-            port = std::stoi(req.get_param_value("IPCPort"));
-            LOG(INFO) << BLUE <<"Got heartbeat from port " << port << NONE;
-        } catch (const std::exception&) {
-            res.status = 400;  // Bad Request
-            return;
-        }
-        auto new_info = HeartbeatInfo{ req.get_param_value("abilityName"), std::stoi(req.get_param_value("IPCPort")), std::stoi(req.get_param_value("abilityPort")), req.get_param_value("status"),  std::chrono::steady_clock::now()};
-        this->lifecycle_manager_->AddHeartbeatInfo(new_info);
-    });
+    this->server->Post("/heartbeat",
+                       [this](const httplib::Request &req, httplib::Response &res)
+                       {
+                           LOG(INFO) << L_BLUE << "httpserver receive: method: Post   URL: /heartbeat" << NONE;
+                           int port;
+                           try
+                           {
+                               port = std::stoi(req.get_param_value("IPCPort"));
+                               LOG(INFO) << BLUE << "Got heartbeat from port " << port << NONE;
+                           }
+                           catch (const std::exception &)
+                           {
+                               res.status = 400; // Bad Request
+                               return;
+                           }
+                           auto new_info = HeartbeatInfo{req.get_param_value("abilityName"), std::stoi(req.get_param_value("IPCPort")), std::stoi(req.get_param_value("abilityPort")), req.get_param_value("status"), std::chrono::steady_clock::now()};
+                           this->lifecycle_manager_->AddHeartbeatInfo(new_info);
+                       });
 
-    this->server->Get("/api/Devices", [this](const httplib::Request &req, httplib::Response &res)
-    { 
-        LOG(INFO) << L_BLUE << "httpserver receive devices" << NONE;
-        res.set_content(this->resource_manager_->GetHardwareDeviceInfo(true), "application/json"); 
-    });
+    this->server->Get("/api/Devices",
+                      [this](const httplib::Request &req, httplib::Response &res)
+                      {
+                          LOG(INFO) << L_BLUE << "httpserver receive: method: GET   URL: /api/Devices" << NONE;
+                          res.set_content(this->resource_manager_->GetHardwareDeviceInfo(true), "application/json");
+                      });
 
-    this->server->Get("/api/AbilityRunning", [this](const httplib::Request &req, httplib::Response &res)
-    { 
-        LOG(INFO) << L_BLUE << "httpserver receive abilityrunning" << NONE;
-        res.set_content(this->lifecycle_manager_->GetHeartbeatMap(), "application/json"); 
-    });
+    this->server->Get("/api/AbilityRunning",
+                      [this](const httplib::Request &req, httplib::Response &res)
+                      {
+                          LOG(INFO) << L_BLUE << "httpserver receive: method: GET   URL: /api/AbilityRunning" << NONE;
+                          res.set_content(this->lifecycle_manager_->GetHeartbeatMap(), "application/json");
+                      });
 
-    this->server->Get("/api/AbilitySupport", [this](const httplib::Request &req, httplib::Response &res)
-    {
-        LOG(INFO) << L_BLUE << "httpserver receive abilitysupport" << NONE;
-        res.set_content(this->relation_manager_->GetAbilitySupport(), "application/json"); 
-    });
+    this->server->Get("/api/AbilitySupport",
+                      [this](const httplib::Request &req, httplib::Response &res)
+                      {
+                          LOG(INFO) << L_BLUE << "httpserver receive: method: GET   URL: /api/AbilitySupport" << NONE;
+                          res.set_content(this->relation_manager_->GetAbilitySupport(), "application/json");
+                      });
 
-    this->server->Post("/api/AbilityRequest", [this](const httplib::Request &req, httplib::Response &res)
-    {
-        LOG(INFO) << L_BLUE << "httpserver receive abilityrequest" << NONE;
-        auto cmd_g = CommandInfo{std::stoi(req.get_param_value("IPCPort")),
-                                req.get_param_value("abilityName"),
-                                req.get_param_value("cmd"),
-                                std::stoi(req.get_param_value("connectPort")),
-                                req.get_param_value("connectIP")};
-        this->lifecycle_manager_->HandleCommandInfo(cmd_g);
-    });
-
-    this->server->Post("/cloudaddress",[this](const httplib::Request &req, httplib::Response &res)
-    {
-        std::string address = req.get_param_value("address");
-        std::string type = req.get_param_value("protocol");
-        ProtocolType des_type;
-        if (type == "TCP")
-        {
-            des_type = ProtocolType::TCP;
-        }
-        else if (type == "UDP")
-        {
-            des_type = ProtocolType::UDP;
-        }
-        auto connect_info = ConnectInfo{address,des_type,ProtocolTendency::Random,ConnectionStatus::Uninitialized};
-        this->connection_manager_->OnCloudAddressRecordChange(connect_info);
-    });
+    this->server->Post("/api/AbilityRequest",
+                       [this](const httplib::Request &req, httplib::Response &res)
+                       {
+                           LOG(INFO) << L_BLUE << "httpserver receive: method: POST   URL: /api/AbilityRequest" << NONE;
+                           this->lifecycle_manager_->HandleCommandInfo(req.body);
+                       });
 }
 
 void HttpServer::Run()
