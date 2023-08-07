043408bdf59637a177074c766b95a597a9125b0b
1180300515 <1516648631@qq.com>
2023-08-07 20:18:53 +0800

    测试文件修改

--- test/test_post_abilityrequest.cpp
+++ test/test_post_abilityrequest.cpp
@@ -2,6 +2,7 @@
 
 #include <httplib.h>
 
+#include "json/json.h"
 
 int main(int argc, char **argv)
 {
@@ -13,14 +14,15 @@ int main(int argc, char **argv)
 
     httplib::Client ccc("localhost", 8080);
     httplib::Params params;
+    Json::Value data;
 
-    
-    params.emplace("abilityName",argv[1]);
-    params.emplace("IPCPort",argv[2]);
-    params.emplace("cmd",argv[3]);
-    params.emplace("connectPort",argv[4]);
-    params.emplace("connectIP",argv[5]);
-    auto res = ccc.Post("/api/AbilityRequest", params);
+    data["abilityName"] = argv[1];
+    data["IPCPort"] = std::stoi(argv[2]);
+    data["cmd"] = argv[3];
+    data["connectPort"] = std::stoi(argv[4]);
+    data["connectIP"] = argv[5];
+    Json::FastWriter writer;
+    auto res = ccc.Post("/api/AbilityRequest", writer.write(data), "application/json");
     if (res)
     {
         std::cout << res->body << std::endl;
