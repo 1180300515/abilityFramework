d4dadfcc366bbedebfe4bf37fc3315c65f883407
1180300515 <1516648631@qq.com>
2023-08-07 21:17:07 +0800

    测试代码完善

--- test/test_post_abilityrequest.cpp
+++ test/test_post_abilityrequest.cpp
@@ -13,16 +13,26 @@ int main(int argc, char **argv)
     }
 
     httplib::Client ccc("localhost", 8080);
-    httplib::Params params;
-    Json::Value data;
 
-    data["abilityName"] = argv[1];
-    data["IPCPort"] = std::stoi(argv[2]);
-    data["cmd"] = argv[3];
-    data["connectPort"] = std::stoi(argv[4]);
-    data["connectIP"] = argv[5];
-    Json::FastWriter writer;
-    auto res = ccc.Post("/api/AbilityRequest", writer.write(data), "application/json");
+    // Json::Value data;
+    // data["abilityName"] = argv[1];
+    // data["IPCPort"] = std::stoi(argv[2]);
+    // data["cmd"] = argv[3];
+    // data["connectPort"] = std::stoi(argv[4]);
+    // data["connectIP"] = argv[5];
+    // Json::FastWriter writer;
+    // auto res = ccc.Post("/api/AbilityRequest", writer.write(data), "application/json");
+    // if (res)
+    // {
+    //     std::cout << res->body << std::endl;
+    // }
+    httplib::Params params;
+    params.emplace("abilityName", argv[1]);
+    params.emplace("IPCPort", argv[2]);
+    params.emplace("cmd", argv[3]);
+    params.emplace("connectPort", argv[4]);
+    params.emplace("connectIP", argv[5]);
+    auto res = ccc.Post("/api/AbilityRequest", params);
     if (res)
     {
         std::cout << res->body << std::endl;
